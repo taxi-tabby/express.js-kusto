664db8848fc3a5d66e91e29afb52c92bc9fb10ef
Taxi_tabby <rkdmf0000@gmail.com>
2025-05-31 13:20:09 +0900

    fix: update serve script and webpack output filename to use server.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: "server.js",
     },    module: {
         rules: [
             {

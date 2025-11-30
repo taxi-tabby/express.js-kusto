0a20a4f32e8eb96af21de161c7f6673b7fb19757
Taxi_tabby <rkdmf0000@gmail.com>
2025-11-30 20:37:20 +0900

    chore: update prisma dependencies to version 7.0.1 and refactor prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,12 @@
 import 'dotenv/config'
-import type { PrismaConfig } from 'prisma'
+import { defineConfig, env } from 'prisma/config'
 
-export default {
-
-} satisfies PrismaConfig
+export default defineConfig({
+//   schema: 'src/app/db/default/schema.prisma',
+//   migrations: {
+//     path: 'src/app/db/default/migrations',
+//   },
+//   datasource: {
+//     url: env('RDS_URL'),
+//   },
+})
